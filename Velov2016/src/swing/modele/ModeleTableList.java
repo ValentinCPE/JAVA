964c55665101f270df1986ae9b133318c879b515
Valentin <valentin.guevara@etu.univ-lyon1.fr>
2016-06-02 13:21:23 +0200

    creation modele table

--- Velov2016/src/swing/modele/ModeleTableList.java
+++ Velov2016/src/swing/modele/ModeleTableList.java
@@ -5,10 +5,48 @@
  */
 package swing.modele;
 
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Valentin
  */
-public class ModeleTableList {
+public class ModeleTableList extends AbstractTableModel{
+    private String[] columnNames = {"Numéro station","Nom station","Numéro arrondissement", "Localisation"};
+    Object donnees[][];
+    
+    public ModeleTableList(Object donne[][]){
+        this.donnees = donne;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return donnees.length; 
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length; 
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return donnees[rowIndex][columnIndex];  
+}
+    @Override
+    public boolean isCellEditable(int row, int col) {
+        if (col < 2) {
+        return false;
+        } else {
+        return true;
+       }
+}
+    @Override
+    public void setValueAt(Object value, int row, int col) {
+        donnees[row][col] = value;
+        fireTableCellUpdated(row, col);
+        }
     
 }
--- Velov2016/src/velov/vue/Velov.java
+++ Velov2016/src/velov/vue/Velov.java
@@ -4,6 +4,8 @@ import java.awt.Component;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import javax.swing.JTable;
+import swing.modele.ModeleTableList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,11 +18,13 @@ import java.io.ObjectOutputStream;
  * @author Valentin
  */
 public class Velov extends javax.swing.JFrame {
-
+    
     /**
      * Creates new form Velov
      */
     public Velov() {
+        this.mm = new ModeleTableList(donnees, titreColonnes);
+        this.table1 = new JTable(new ModeleTableList());
         initComponents();
     }
 
@@ -36,8 +40,6 @@ public class Velov extends javax.swing.JFrame {
         d = new javax.swing.JFileChooser();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
@@ -60,19 +62,6 @@ public class Velov extends javax.swing.JFrame {
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/velov/vue/bandeau.JPG"))); // NOI18N
         jLabel1.setText("jLabel1");
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable1);
 
         jLabel2.setText("Numéro station :");
 
@@ -113,19 +102,17 @@ public class Velov extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(0, 65, Short.MAX_VALUE)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 564, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 65, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 564, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(22, 22, 22))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(51, 51, 51)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -144,9 +131,7 @@ public class Velov extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(151, 151, 151)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -269,12 +254,15 @@ public class Velov extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new Velov().setVisible(true);
             }
         });
     }
 
+    ModeleTableList mm;     
+    private JTable table1;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JFileChooser d;
     private javax.swing.JButton jButton1;
@@ -292,9 +280,7 @@ public class Velov extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
